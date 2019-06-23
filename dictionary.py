4b96d8d6f06b16e4346f2d530fe9f3c00e665c7e
Rishabh Arya <rishabharya2799@gmail.com>
2019-06-24 01:04:42 +0530

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,23 @@
 import json
-from fuzzywuzzy import fuzz
-#from fuzzywuzzy import process
-
+import difflib
 datastore = json.load(open("data.json", "r"))
 word = input("Enter the word to be searched:- ")
 word = word.lower()
-'''if word in datastore.keys():
+if word in datastore.keys():
     print(word, "-", "\n", datastore[word])
 else:
-    print("No meaning found")'''
-for key in datastore:
-    ratio = fuzz.partial_ratio(word, key)
-    if (ratio == 100):
-
-
-
+    possibilities = difflib.get_close_matches(word, datastore, n=3, cutoff=0.6)
+    print("Do you mean")
+    num = 1
+    for key in possibilities:
+        print(num,key)
+        num = num + 1
+    print("4 No One")
+    opt = input()
+    opt = int(opt)
+    if opt == 4:
+        print("No meaning found")
+    else:
+        opt = opt - 1
+        word = possibilities[opt]
+        print(word, "-", "\n", datastore[word])
